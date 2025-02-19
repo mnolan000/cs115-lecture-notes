06aba71229d9405f0af1a4d18aa0c823a2f32fec
mnolan000 <matthewrosenolan@gmail.com>
2025-02-19 19:15:13 +0000

    We've added to main.py a bunch of examples on how to code in Python

---
+++ main.py
@@ -0,0 +1,36 @@
+print("Hello World!")
+
+my_variable = 6
+num_cats = 8
+
+print(num_cats)
+print("num_cats")
+
+print("num_cats: ", num_cats)
+print(f"number of cats {num_cats}")
+print(f"my age {my_variable} and my cats {num_cats}")
+
+#answer = input("What is your name? ")
+#print(f"You answered: {answer}")
+
+x = 6
+y = 20
+print(f"x = {x}")
+c = x + y
+print(c)
+
+print(f"c = {x+y}")
+print(f"x*y = {x*y}")
+print(f"x/y = {x/y}")
+print(f"x**y = {x**y}")
+
+"""
+This is a multi-line comment
+Anything in here will not be run
+"""
+# This is a single line comment
+
+number1 = input("What is the first number? ")
+number2 = input("What is the second number? ")
+sum = int(number1) + int(number2)
+print(f"{number1} + {number2} = {sum}")
